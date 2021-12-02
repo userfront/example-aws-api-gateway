a8e483856e5e0a9690d636d79ccd22a0ea62592b
Tyler <tyler@anymod.com>
2021-12-02 14:29:52 -0800

    Make handler sync

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const jwt = require("jsonwebtoken");
 
 const jwtPublicKey = `-----BEGIN RSA PUBLIC KEY-----
-MIIBCgKCAQEAp6ZRhF+bXypVlJEDL2v/kEL4Mo9tx3vUOitMhxPDA1JCZs2TGqN0
-LxJdDbCWPaIbgTfqwdmVlC7l2tTQtJhC48TzF8BEjY2341/jBVY0z1D8/v2rpSw8
-eGLIhyVH/sT+/7F+Arh0HA62ctvrnYIie+vsnDiMrjiMwTpuzcVAGwT5PaaU9bu9
-e7oes7jX3jI/jI76llicbZgbTXijWLp/V0WBbuWatvFfBzqBmBQNGGb8hDEDM0Qu
-zY7KiOddKCwbdGgM1DAl/AjVS3Bfu9f9epAaVt2i2XTCUSHOi+0iaoQCMn9hhncF
-CWlWZ9GwwhMecueXwSBrtuwp1i+1RtwcfQIDAQAB
+MIIBCgKCAQEAodD/IEagav7wlBX+k30YOSFpYT0u7AtV3ljwC52ShCFFGVvw86T5
+VTbg5Q/L/dgQT0+OZi+Fe/aAIL6j+3d8+Md5nGg7zqTv33GE7tN4ZoSkYnPMAm1I
+PjkOevpia98u8n1jWE/OnDnQqgozcy2zssGcJ1+QwJWuZWVObbFiA6ppFlyb9Hm8
+2wEgvBqjuTqCvLdJO5CtY5ya5OpGLpnqlsXTRgJEEFk0VTdH56ztcLFMDMxm4OVW
+aWy+i4YieTRRKnbyT7fzDPiZupkcg2jwVF49CtyB9UWtE/+/BAKtJtBLfdZ5X1dK
+RqesE10ysVdGxeyeRpyFltEfF5QWAzn99wIDAQAB
 -----END RSA PUBLIC KEY-----`;
 
 // This handler validates the token
-exports.handler = async (event, context, callback) => {
-  console.log("invoked");
+exports.handler = (event, context, callback) => {
+  console.log(`Run ${Math.random()}`);
+  if (!event.authorizationToken) {
+    return callback("Unauthorized");
+  }
   const accessToken = event.authorizationToken.replace(/^Bearer /, "");
-  console.log("Token: ", accessToken);
+  console.log(`Token:${accessToken}`);
 
   jwt.verify(
     accessToken,
     jwtPublicKey,
     { algorithms: ["RS256"] },
     (error, decoded) => {
+      console.log(error);
       if (error) {
         callback("Unauthorized");
       } else {
